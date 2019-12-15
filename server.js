29bd59bd74fff655d2a2f44b821c456c79342619
Ahmar Suhail <ahmar.suhail@gmail.com>
2019-12-15 12:50:05 +0000

    changes for deployment

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(errorHandler);
 
 // start server
 const port = 4000;
-const server = app.listen(port, function () {
+const server = app.listen(process.env.PORT || port, function () {
     console.log('Server listening on port ' + port);
 });
